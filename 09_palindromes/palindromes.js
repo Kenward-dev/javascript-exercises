c3cf5bd889c4ea8bf2e2fe3e59251ff02ea4ed98
Kenward Terhemba <codewithkenward@gmail.com>
2024-10-13 10:57:09 +0100

    update

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normalizedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+    let reversedStr = normalizedStr.split('').reverse().join('');
+    return normalizedStr === reversedStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (typeof n === 'string') {
+        n = parseInt(n, 10);
+    }    
+    // n = typeof n === 'string' ? parseInt(n, 10) : n;
+    if (n < 0) return 'OOPS';
+    if(n === 0) return 0;
+    let a = 0, b = 1, result;
+    for (let i = 1; i < n; i++) {
+        result = a + b
+        a = b
+        b = result
+    }
+    return b;
 };
 
 // Do not edit below this line
